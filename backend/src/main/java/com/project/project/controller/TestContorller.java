15ede9cd9813a48a277ec67f48ee5bd96bd2b783
Unknown <stefan.varga98@gmail.com>
2019-03-24 17:35:47 +0100

    init

---
+++ backend/src/main/java/com/project/project/controller/TestContorller.java
@@ -0,0 +1,18 @@
+package com.project.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestContorller {
+
+    @RequestMapping(
+            value = "/api/hello",
+            method = RequestMethod.GET
+    )
+    public String hello() {
+        return "Hello Spring";
+    }
+}
