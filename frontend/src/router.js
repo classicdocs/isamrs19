3f66a406fc72a526b54c309c6c810336f2b3c5fa
Unknown <stefan.varga98@gmail.com>
2019-04-19 22:47:45 +0200

    small fix

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -36,7 +36,7 @@ export default new Router({
       path: "/flight-reservation",
       name: "flight-reservation",
       component: FlightReservation
-    }
+    },
     {
       path: "/registration",
       name: "/registration",
