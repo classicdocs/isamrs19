91fcd37fac2f5f3befd3ad1aed84d29fd59e4018
Unknown <stefan.varga98@gmail.com>
2019-06-17 15:33:32 +0200

    test deploy

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -2,14 +2,17 @@ import {ENVIRONMENTS, BACKEND_HOSTNAMES, API_ROUTE} from "@/constants";
 
 export default {
   getEnv() {
+    console.log(window.location.hostname);
     return ENVIRONMENTS[window.location.hostname];
   },
 
   getHostName() {
-    return BACKEND_HOSTNAMES[this.getEnv];
+    console.log(this.getEnv());
+    return BACKEND_HOSTNAMES[this.getEnv()];
   },
 
   getApiUrl() {
+    console.log(this.getHostName());
     return this.getHostName() + API_ROUTE;
   }
 };
