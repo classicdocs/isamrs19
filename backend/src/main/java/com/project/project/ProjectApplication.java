f08bb84ef505c4ab41ff1264f785be69d1ae3812
Unknown <stefan.varga98@gmail.com>
2019-06-20 20:41:30 +0200

    final

--- backend/src/main/java/com/project/project/ProjectApplication.java
+++ backend/src/main/java/com/project/project/ProjectApplication.java
@@ -2,15 +2,31 @@ package com.project.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaRepositories
-public class ProjectApplication {
+@Controller
+public class ProjectApplication implements ErrorController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+	private static final String PATH = "/error";
+
+	@RequestMapping(value = PATH)
+	public String error() {
+		return "forward:/index.html";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return PATH;
 	}
 }
